59d4cd62b02b6b57c4529d3bbff0f67bb0452220
110B13203 <112997321+110B13203@users.noreply.github.com>
2022-11-07 22:04:55 +0800

    Add files via upload

---
+++ Class05.java
@@ -0,0 +1,35 @@
+interface Math{
+	public abstract void show();
+	public abstract void add(int a,int b);//a+b
+	public abstract void sub(int a,int b);//a-b
+	public abstract void mul(int a,int b);//a*b
+	public abstract void div(int a,int b);//a/b
+}
+class Compute implements Math{
+	int ans;
+	public void add(int a,int b) {
+		ans=a+b;
+	}
+	public void sub(int a,int b) {
+		ans=a-b;
+	}
+	public void mul(int a,int b) {
+		ans=a*b;
+	}
+	public void div(int a,int b) {
+		ans=a/b;
+	}
+	public void show() {
+		System.out.println("ans="+ans);
+	}
+	
+}
+public class Class05 {
+
+	public static void main(String[] args) {
+		Compute cmp=new Compute();
+		cmp.mul(3,5);
+		cmp.show();
+	}
+
+}
